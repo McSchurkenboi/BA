1b872107f2189c192a167ac9cdf19bec95419e10
McSchurkenboi <felixritter@me.com>
2018-11-05 14:40:28 +0100

    Annie laden

--- src/main/java/AnnieHandler.java
+++ src/main/java/AnnieHandler.java
@@ -1,15 +1,68 @@
+
+import gate.Corpus;
+import gate.CorpusController;
+import gate.Factory;
+import gate.Gate;
+import gate.creole.ANNIEConstants;
+import gate.creole.Plugin;
+import gate.creole.ResourceReference;
+import gate.creole.SerialAnalyserController;
+import gate.util.GateException;
+import gate.util.persistence.PersistenceManager;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
-
-/**
- *
  * @author rittfe1
  */
 public class AnnieHandler {
-    public void init(){
-        
+
+    public void init() {
+        try {
+            // initialise the GATE library
+            Gate.init();
+
+            // load the ANNIE plugin
+            Plugin anniePlugin = new Plugin.Maven("uk.ac.gate.plugins", "annie", gate.Main.version);
+            Gate.getCreoleRegister().registerPlugin(anniePlugin);
+
+            // load ANNIE application from inside the plugin
+            SerialAnalyserController controller = (SerialAnalyserController) PersistenceManager.loadObjectFromUrl(new ResourceReference(
+                    anniePlugin, "resources/" + ANNIEConstants.DEFAULT_FILE)
+                    .toURL());
+            
+            
+        } catch (GateException | URISyntaxException | IOException ex) {
+            Logger.getLogger(AnnieHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void initFromRestore() {
+        try {
+            Gate.init();
+            CorpusController controller = (CorpusController) PersistenceManager.loadObjectFromFile(new File("C:\\Users\\rittfe1\\Desktop\\gateSachen\\MyANNIE.gapp"));
+
+            Corpus corpus = Factory.newCorpus("MyCorpus");
+            controller.setCorpus(corpus);
+            
+            corpus.add(Factory.newDocument(new File("C:\\Users\\rittfe1\\Desktop\\gateSachen\\Lastenheft.txt").toURI().toURL()));
+            
+            controller.execute();
+            
+            corpus.clear();
+                        
+        } catch (GateException ex) {
+            System.out.println("Gate-Fehler");
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            Logger.getLogger(AnnieHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
